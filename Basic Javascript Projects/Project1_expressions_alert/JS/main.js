0d52e9cf72105710fca58ca78235a4b64ec6e63c
lellithan <faulhaberellie1@gmail.com>
2024-03-01 21:04:37 -0800

    html and js created
    index created and linked to my main.js. lots of new code testing in our main, comments added.

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+document.write('Hello, World! '); //Write to our html doc the string
+var A = "This is a string. "; // Decalre our variable A as a string
+document.write(A); // Write the string instatiated in A to the html doc
+window.alert(A); // A popup that shows the contents of A
+var B = "Here's some apostrophes \" \" and a backslash \\. "; // Using backslash to display characters that have syntactical purpose
+document.write(B); // Write the string
+document.write("1, "+ "2, " + "3. ") // Directly write a concatenated string
+var C = "I really like " + "pepperjack cheese. " + "It's my favorite.";
+document.write(C);// Declaring a variable using concatentated strings
+var Name = "Ellie", Age = "26", Dog = "Yes"; // Declaring mutliple variables at once
+document.write(Dog); // Write one of our instantiated variables
+document.write(3 + 4); // An expression within the write method
+
+// Basic JavaScript Pojects Submission Assignemnt
+
+// 1. alert utilized on line 4 and write on line 1.
+
+// 2.
+var Sent1 = "This is the beginning of the sttring", Sent2 = " and this is the end of the string.";
+
+// 3. 
+document.write(Sent1 + Sent2);
+
+// 4. expression written on line 12
+
+// 5.  see comments after each line :D
+
+// 6. see index.html <head> element
